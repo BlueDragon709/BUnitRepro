bbd5087c7eb3b97a7a87b23acb6d328456549b01
BlueDragon <briansoetens123@gmail.com>
2025-03-20 15:22:03 +0100

    Add project files.

---
+++ BUnitRepro.Client.Tests/TestPageTests.cs
@@ -0,0 +1,52 @@
+﻿using BUnitRepro.Client.Tests.Components;
+
+namespace BUnitRepro.Client.Tests
+{
+    public class TestPageTests : Bunit.TestContext
+    {
+        private readonly IRenderedComponent<TestPage> _page;
+
+        public TestPageTests()
+        {
+            _page = RenderComponent<TestPage>();
+        }
+
+        [Fact]
+        public void FindWorkingComponent()
+        {
+            var component = _page.FindComponents<WorkingComponent>();
+
+            Assert.NotNull(component);
+        }
+
+        [Fact]
+        public void FindSubComponent()
+        {
+            var component = _page.FindComponents<SubComponent>();
+
+            Assert.NotNull(component);
+        }
+
+        [Fact]
+        public void FindBothInInheritanceOrder()
+        {
+            var subComponent = _page.FindComponents<SubComponent>();
+            var topLevelComponent = _page.FindComponents<WorkingComponent>();
+
+            Assert.NotNull(subComponent);
+            Assert.NotNull(topLevelComponent);
+        }
+
+        [Fact]
+        public void FindBothInReverseInheritanceOrder()
+        {
+            var topLevelComponent = _page.FindComponents<WorkingComponent>();
+            // Every search of derived components of WorkingComponent after this point wont be able to cast to derived component
+            // same for HasComponent<T>()
+            var subComponent = _page.FindComponents<SubComponent>();
+
+            Assert.NotNull(subComponent);
+            Assert.NotNull(topLevelComponent);
+        }
+    }
+}
